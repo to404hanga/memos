26db7e3b99d806f00bcce60d6151517e407d3a76
to404hanga <110234946+to404hanga@users.noreply.github.com>
2026-05-24 13:36:26 +0800

    Chore/webpack production mode (#11)
    * fix(dev): 修复dev模式下主进程构建模式错误
    
    * docs: 更新项目架构与开发指南说明

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'production',
   target: 'electron-main',
   entry: './src/main/index.ts',
   output: {
